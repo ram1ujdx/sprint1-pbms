497618e2c66ff3c6506bae3fdcdec2549b49231b
ramanujdas <ramanujds9@gmail.com>
2020-02-17 14:59:27 +0530

    project structure created

---
+++ pecunia-banking-management-system/src/main/java/com/capg/sprint1/pecunia_banking_management_system/App.java
@@ -0,0 +1,13 @@
+package com.capg.sprint1.pecunia_banking_management_system;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
